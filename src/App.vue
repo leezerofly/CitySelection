737202017155d417524d4c5f087430fb03841081
Zhenfei.Li <leezerofly@gmail.com>
2018-07-09 21:13:22 +0800

    搜索框引入

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <el-button>默认按钮</el-button>
+    <el-container>
+      <el-header>
+        <Search/>
+      </el-header>
+      <el-main>
+        <img src="./assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <el-button>默认按钮</el-button>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Search from './components/Search.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Search
   }
 }
 </script>
